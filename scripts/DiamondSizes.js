2f591bed409b4c8a3d67b017fa925dd713a1974e
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-07-31 01:10:40 -0500

    added functionality- HTML regeneration

--- scripts/DiamondSizes.js
+++ scripts/DiamondSizes.js
@@ -1,4 +1,6 @@
-import { getSizes, setSize } from "./database.js"
+import { getSizes, setSize, getOrderBuilder} from "./database.js"
+import { renderAllHTML } from "./main.js"
+
 
 const sizes = getSizes()
 
@@ -7,21 +9,30 @@ document.addEventListener(
     (event) => {
         if (event.target.name === "size") {
             setSize(parseInt(event.target.value))
+            console.log("State of data has changed. Regenerating HTML...")
+            renderAllHTML()
         }
     }
 )
 
 export const DiamondSizes = () => {
+    const orderBuilder = getOrderBuilder()
     let html = "<ul>"
 
     // Use .map() for converting objects to <li> elements
-    const listItemsArray = sizes.map(size => {
-        return `<li>
-            <input type="radio" name="size" value="${size.id}" /> ${size.carets}
-        </li>`
+    const listItems = sizes.map(size => {
+        if (size.id === orderBuilder.sizeId) {
+            return `<li>
+            <input type="radio" name="size" value="${size.id}" checked="checked"/>${size.carets}
+            </li>`
+        } else {
+            return `<li>
+            <input type="radio" name="size" value="${size.id}" />${size.carets}
+            </li>`
+        }
     })
 
-    html += listItemsArray.join(" ")
+    html += listItems.join(" ")
     html += "</ul>"
 
     return html
