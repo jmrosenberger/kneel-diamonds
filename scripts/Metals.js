c30395da6d61f83d7a936afc36a5eced37e9aa23
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-07-31 01:24:57 -0500

    added HTML regeneration functionality

--- scripts/Metals.js
+++ scripts/Metals.js
@@ -1,4 +1,6 @@
-import { getMetals, setMetal } from "./database.js"
+import { getMetals, setMetal, getOrderBuilder } from "./database.js"
+import { renderAllHTML } from "./main.js"
+
 
 const metals = getMetals()
 
@@ -9,20 +11,30 @@ document.addEventListener(
     (event) => {
         if (event.target.name === "metal") {
             setMetal(parseInt(event.target.value))
+            console.log("State of data has changed. Regenerating HTML...")
+            renderAllHTML()
         }
     }
 )
 
 export const Metals = () => {
+    const orderBuilder = getOrderBuilder()
+
     let html = "<ul>"
 
     // This is how you have been converting objects to <li> elements
-    for (const metal of metals) {
-        html += `<li>
-            <input type="radio" name="metal" value="${metal.id}" /> ${metal.metal}
-        </li>`
-    }
-
+    const listItems = metals.map(metal => {
+        if (metal.id === orderBuilder.metalId) {
+            return `<li>
+            <input type="radio" name="metal" value="${metal.id}" checked="checked"/>${metal.metal}
+            </li>`
+        } else {
+            return `<li>
+            <input type="radio" name="metal" value="${metal.id}" />${metal.metal}
+            </li>`
+        }
+    })
+    html += listItems.join("")
     html += "</ul>"
     return html
 }
